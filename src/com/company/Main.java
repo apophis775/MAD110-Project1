6f4748d219671a9ae96614fce33ef551802c5d53
apophis775 <apophis775@gmail.com>
2017-09-03 22:42:48 -0500

    First Commit
    WOOT.

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("We are supposed to write a few lines about ourselves.");
+	System.out.println("Personally, I prefer GitLab to GitHub.");
+	System.out.println("The main reason, is because along with SSH, it allows 2-factor authentication through the Google Authenticator");
+	System.out.println("Also, there's a lot more plugins available, for example linking with Trello.");
+	System.out.println("Though, for the online game I wrote, We used GitHub for a really long time under Private Repos");
+	System.out.println("As a result, I've already got Git for windows installed, and I'm fairly familiar with it and other Git programs.");
+    }
+}
